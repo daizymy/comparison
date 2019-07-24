11a85b6669de22f0a140cf7e3f015809c2365547
daizymy <liliyapyak@yahoo.com>
2019-07-24 09:35:24 +0200

    table with maxHeight for features

--- js/ba-comparsion.js
+++ js/ba-comparsion.js
@@ -1,122 +1,299 @@
 var $ = jQuery;
+function truncate(input, max) {
+    if (input.length > max)
+        return input.substring(0,max) + '...';
+    else
+        return input;
+};
 
 $(document).ready(function() {
 
-  $("#comparsionStart").click(function(){
-    var products = [];
-    var features = {};
-    var productFeatures = {};
-    var productCodeArr = $("#compareItems").val().split(",");
 
-    productCodeArr.forEach(function(productCode) {
-        getProductData(productCode, products, features, productFeatures);
-    });
+    var headerOffset = null;
+    var comparisonListHeader =  $('#comparisonListHeader');
 
-    window.console.log(products, features, productFeatures);
-
-    // Render 1st column
-    $('#comparisonFeatures').html('');
-    $('#comparisonProducts1').html('');
-    $('#comparisonProducts2').html('');
-
-    var html = '<ul class="product">' +
-        '<li class="compHeader">' +
-          '<p class="w3-display-middle">Features</p>' +
-        '</li>';
-
-    for(var featureCode in features) {
-       html = html + '<li>' + features[featureCode] + '</li>';
-    }
-
-    html = html + '</ul>';
-
-    $('#comparisonFeatures').append(html);
-
-    // Render product columns
-    products.forEach(function(product, index) {
-
-      html =
-        '<div class="carousel-item' + (index == 0 ? ' active' : '') + '">' +
-            '<ul class="product">' +
-                '<li class="compHeader">' +
-                    '<p class="w3-display-middle">' +
-                      '<img src="' + product.image + '" />' +
-                      '<b>' + product.manufacturer + '</b> ' + product.name + '<br />' +
-                      product.price + '<br />' +
-                    '</p>' +
-                '</li>';
-
-        for(var featureCode in features) {
-           var featureValue = '-';
-           if (productFeatures.hasOwnProperty(product.code + '_' + featureCode)) {
-             featureValue = productFeatures[product.code + '_' + featureCode];
-           }
-           html = html + '<li>' + featureValue + '</li>';
+    $(window).scroll(function() {
+        if (headerOffset !== null) {
+            var windowOffset = headerOffset - $(window).scrollTop();
+            var $lastProduct = $('#comparisonProducts1 .carousel-item.active .product-link').first();
+            var lastProductOffset = $lastProduct.offset().top;
+            var bottomOffset = lastProductOffset - $(window).scrollTop();
+            var showSticky = windowOffset <= 0 && bottomOffset > 210;
+            comparisonListHeader.toggleClass('visible', showSticky);
         }
-      html = html + '</ul></div>';
-
-      $('#comparisonProducts1').append(html);
-      $('#comparisonProducts2').append(html);
-
-
-
-
-
 
     });
-    var indicators1 = $("#comparisonCarousel1 .carousel-indicators");
-    var bootCarousel = $("#comparisonCarousel1");
-    //// TODO: bullet points id change comparsionCarousel1
-    console.log(bootCarousel.find(".carousel-inner").children(".carousel-item"));
-    bootCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
-      console.log(index);
-      (index === 0) ?
-      indicators1.append("<li data-target='#MyCarousel' data-slide-to='" + index + "' class='active'></li>") :
-      indicators1.append("<li data-target='#MyCarousel' data-slide-to='" + index + "'></li>");
-    });
-    //indicators.append("<li data-target='#comparisonCarousel1' data-slide-to='" + index + "'></li>");
-  });
 
-  function getProductData(productCode, products, features, productFeatures) {
-    $.ajax({
-  		url: "https://www.babyartikel.de/kpfrest/v2/babyartikel/products/" + productCode + "?fields=FULL&accept=application/json",
-  		type: "GET",
-      async: false,
-  		contentType: 'application/json; charset=utf-8',
-  		success: function(productData) {
 
-        var whitelist = [
-          'kinderwagen-abmessungen-gewicht-abmessungen',
-          'kinderwagen-abmessungen-gewicht-gewicht',
-          'kinderwagen-fahrwerk-anzahl-raeder',
-          // @todo: add more attributes
-        ];
+    $("#comparsionStart").click(function(){
+        $("#comparisonListWrapper").show();
+        var products = [];
+        var features = {};
+        var productFeatures = {};
+        var productCodeArr = $("#compareItems").val().split(",");
 
-        products.push({
-          'code': productData.code,
-          'name' : productData.name,
-          'image' : productData.images[0].url,
-          'manufacturer' : productData.manufacturer,
-          'price': productData.price.formattedValue,
+
+        productCodeArr.forEach(function(productCode) {
+            getProductData(productCode, products, features, productFeatures);
         });
 
-        if (productData.hasOwnProperty('classifications')) {
-          productData.classifications.forEach(function(classification) {
-            classification.features.forEach(function(feature) {
-              var featureCode = feature.code.split('/');
-                featureCode = featureCode[featureCode.length - 1];
-                featureCode = featureCode.replace(/\./g,"-").replace(/\//g,"-");
 
-                if (whitelist.indexOf(featureCode) !== -1 && feature.featureValues[0]) {
-                  features[featureCode] = feature.name;
-                  productFeatures[productData.code + '_' + featureCode] = feature.featureValues[0].value;
-                }
+
+
+
+        // window.console.log(products, features, productFeatures);
+
+        //Reset all containers
+        $('#comparisonProducts1').html('');
+        $('#comparisonProducts2').html('');
+        $('#comparisonHeaderProducts1').html('');
+        $('#comparisonHeaderProducts2').html('');
+
+
+
+        var html;
+        var headerHtml;
+
+        // Render product columns
+        products.forEach(function(product, index) {
+            //Sticky header
+            headerHtml =
+                '<div class="carousel-item' + (index == 0 ? ' active' : '') + '">' +
+                '<span class="product-image"><img src="' + product.image + '" /></span>' +
+                '<span class="product-brand">' + product.manufacturer + '</span>' +
+                '<span class="product-name">' +truncate(product.name,55) + '</span>' +
+                '<span class="product-price">'+ product.price + '</span>' +
+                '</div>';
+
+
+            $('#comparisonHeaderProducts1').append(headerHtml);
+            $('#comparisonHeaderProducts2').append(headerHtml);
+
+
+            $('#comparisonHeaderProducts2').carousel({
+                interval: false,
+                ride:false
+            }).carousel(1);
+
+
+            $('#comparisonHeaderProducts1').carousel({
+                interval: false,
+                ride:false
             });
-          });
-        }
-  		},
-  		error : function(jqXHR, textStatus, errorThrown) {},
-  		timeout: 30000,
-  	});
-  }
+
+
+
+
+
+
+//Main Sliders
+            html =
+                '<div class="carousel-item' + (index == 0 ? ' active' : '') + '">' +
+                '<ul class="product-list">' +
+                '<li class="compHeader">' +
+                '<p class="w3-display-middle">' +
+                '<img src="' + product.image + '" />' +
+                '<b>' + product.manufacturer + '</b> ' + '<b>'+product.name+'</b>' + '<br />' +
+                product.price + '<br />' +
+                '</p>' +
+                '</li>';
+
+
+            for(var featureCode in features) {
+                var featureValue = '-';
+
+                if (productFeatures.hasOwnProperty(product.code + '_' + featureCode).length !== 0) {
+                    featureValue = productFeatures[product.code + '_' + featureCode];
+                    if (featureValue == undefined){
+                        featureValue = '-';
+                    }
+                }
+
+                if(features[featureCode].maxHeight <= 55){
+                    html = html + '<li class="product resize-1">' + '<b>';
+
+                } else {
+                    html = html + '<li class="product resize-2">' + '<b>';
+
+                }
+                html= html + features[featureCode].name + '</b>' + '<br>'  + featureValue + '</li>';
+
+            }
+            html = html + '<li class="product-link"><a href="https://www.babyartikel.de' + product.url+'" class="btn btn-primary btn-block">Zum Produkt</a></li>';
+            html = html + '</ul></div>';
+
+
+
+
+            $('#comparisonProducts1').append(html);
+            $('#comparisonProducts1').carousel({
+                interval: false,
+                ride:false
+            });
+            $('#comparisonProducts2').append(html);
+            $('#comparisonProducts2').carousel({
+                interval: false,
+                ride:false
+            }).carousel(1);
+
+            $(".carousel-sync1, .carousel-sync2").swiperight(function() {
+                $(this).carousel('prev');
+            });
+            $(".carousel-sync1, .carousel-sync2").swipeleft(function() {
+                $(this).carousel('next');
+            });
+
+        });
+
+        /**
+         * Create Navigations-indicators;
+         */
+        var bootCarousel = $("#comparisonCarousel1");
+
+        var indicators1 = $("#comparisonCarousel1 .carousel-indicators");
+        indicators1.html('');
+
+        var indicators2 = $("#comparisonCarousel2 .carousel-indicators");
+        indicators2.html('');
+
+        var headerIndicators1 = $("#comparisonHeaderCarousel1 .carousel-indicators");
+        headerIndicators1.html('');
+
+        var headerIndicators2 = $("#comparisonHeaderCarousel2 .carousel-indicators");
+        headerIndicators2.html('');
+
+        bootCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
+            // 1st carousel
+            (index === 0) ?
+                indicators1.append("<li  data-target='#comparisonCarousel1' data-slide-to='" + index + "' class='active'></li>"):
+                indicators1.append("<li  data-target='#comparisonCarousel1' data-slide-to='" + index + "'></li>");
+
+            // 1st header carousel
+            (index === 0) ?
+                headerIndicators1.append("<li  data-target='#comparisonHeaderCarousel1' data-slide-to='" + index + "' class='active'></li>"):
+                headerIndicators1.append("<li  data-target='#comparisonHeaderCarousel1' data-slide-to='" + index + "'></li>");
+
+            // 2nd carousel
+            (index === 1) ?
+                indicators2.append("<li data-target='#comparisonCarousel2' data-slide-to='" + index + "' class='active'></li>") :
+                indicators2.append("<li data-target='#comparisonCarousel2' data-slide-to='" + index + "'></li>");
+
+            // 2nd header carousel
+            (index === 1) ?
+                headerIndicators2.append("<li  data-target='#comparisonHeaderCarousel2' data-slide-to='" + index + "' class='active'></li>"):
+                headerIndicators2.append("<li  data-target='#comparisonHeaderCarousel2' data-slide-to='" + index + "'></li>");
+        });
+
+
+        /**
+         * Sync carousels
+         */
+        $('.carousel-sync1').on('slide.bs.carousel', function (ev) {
+            // get the direction, based on the event which occurs
+            var dir = ev.direction == 'right' ? 'prev' : 'next';
+
+            // get synchronized non-sliding carousels, and make'em sliding
+            $('.carousel-sync1').not('.sliding').addClass('sliding').carousel(dir);
+
+
+        });
+        $('.carousel-sync1').on('slid.bs.carousel', function () {
+            // remove .sliding class, to allow the next move
+            $('.carousel-sync1').removeClass('sliding');
+        });
+
+        $('.carousel-sync2').on('slide.bs.carousel', function (ev) {
+            // get the direction, based on the event which occurs
+            var dir = ev.direction == 'right' ? 'prev' : 'next';
+            // get synchronized non-sliding carousels, and make'em sliding
+            $('.carousel-sync2').not('.sliding').addClass('sliding').carousel(dir);
+        });
+        $('.carousel-sync2').on('slid.bs.carousel', function () {
+            // remove .sliding class, to allow the next move
+            $('.carousel-sync2').removeClass('sliding');
+        });
+
+        // sync clicks on carousel-indicators as well
+        $('.carousel-sync1 .carousel-indicators li').click(function (e) {
+            e.stopPropagation();
+            var goTo = $(this).data('slide-to');
+            $('.carousel-sync1').not('.sliding').addClass('sliding').carousel(goTo);
+        });
+
+        $('.carousel-sync2 .carousel-indicators li').click(function (e) {
+            e.stopPropagation();
+            var goTo = $(this).data('slide-to');
+            $('.carousel-sync2').not('.sliding').addClass('sliding').carousel(goTo);
+        });
+
+        var $firstProduct = $('#comparisonProducts1 .product').first();
+        headerOffset = $firstProduct.offset().top;
+    });
+
+    function getProductData(productCode, products, features, productFeatures) {
+        $.ajax({
+            url: "https://www.babyartikel.de/kpfrest/v2/babyartikel/products/" + productCode + "?fields=FULL&accept=application/json",
+            type: "GET",
+            async: false,
+            contentType: 'application/json; charset=utf-8',
+            success: function(productData) {
+
+                var whitelist = [
+                    'kinderwagen-abmessungen-gewicht-abmessungen',
+                    'kinderwagen-abmessungen-gewicht-gewicht',
+                    'kinderwagen-fahrwerk-anzahl-raeder',
+                    'kinderwagen-abmessungen-gewicht-hoehe',
+                    'kinderwagen-fahrwerk.vorderraeder',
+                    'kinderwagen-material-pflege-textilbezug',
+                    'kinderwagen-material-pflege-material',
+                    'kinderwagen-abmessungen-gewicht-laenge-klappmass',
+                    'kinderwagen-abmessungen-gewicht.laenge-sitzflaeche',
+                    'kinderwagen-ausstattung.babywanne',
+
+
+
+                    // @todo: add more attributes
+                ];
+
+                products.push({
+                    'code': productData.code,
+                    'name' : productData.name,
+                    'image' : productData.images[0].url,
+                    'manufacturer' : productData.manufacturer,
+                    'price': productData.price.formattedValue,
+                    'url': productData.url,
+                });
+
+                if (productData.hasOwnProperty('classifications')) {
+                    productData.classifications.forEach(function(classification) {
+                        classification.features.forEach(function(feature) {
+                            var featureValue = '';
+                            var featureCode = feature.code.split('/');
+                            var featureUnit = "";
+                            if(typeof feature.featureUnit !== "undefined"){
+                                featureUnit=feature.featureUnit.symbol;
+                            }
+                            featureCode = featureCode[featureCode.length - 1];
+                            featureCode = featureCode.replace(/\./g,"-").replace(/\//g,"-");
+                            featureValue = feature.featureValues[0].value + ' ' + featureUnit;
+                            // if (whitelist.indexOf(featureCode) !== -1 && feature.featureValues[0]) {
+                            productFeatures[productData.code + '_' + featureCode] = featureValue;
+                            if(typeof features[featureCode] !== 'undefined'){
+                                //overwrite maxHeight
+                                if(features[featureCode].maxHeight<featureValue.length){
+                                    features[featureCode].maxHeight=featureValue.length;
+                                }
+                            }else{
+                                features[featureCode] = {'maxHeight':featureValue.length,'name':feature.name};
+                            }
+
+                            //}
+                        });
+                    });
+                }
+            },
+            error : function(jqXHR, textStatus, errorThrown) {},
+            timeout: 30000,
+        });
+    }
 });
--- wp-ba-comparsion.php
+++ wp-ba-comparsion.php
@@ -6,10 +6,10 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 /*
 
-Plugin Name: BA product comparison
+Plugin Name: wp_ba_comparetool
 Description: Compare products
 Version: 2019-05-17
-Author: Philipp Paul
+Author: Liliya Pyak
 Author URI: https://www.babyartikel.de/magazin
 
 */
@@ -21,11 +21,11 @@ Author URI: https://www.babyartikel.de/magazin
 
 function wp_ba_comparsion($atts) {
 	$a = shortcode_atts( array (
-        	'variables' => false,
-        	'template' => 'mag-product',
-			'area' => 'area1',
-			'varname' => 'pid',
-    	), $atts );
+		'variables' => false,
+		'template' => 'mag-product',
+		'area' => 'area1',
+		'varname' => 'pid',
+	), $atts );
 
 	if ($a['variables']) {
 		$variables = explode("&",preg_replace('/\s+/', '', $a['variables']));
@@ -35,44 +35,73 @@ function wp_ba_comparsion($atts) {
 		}
 	};
 
-	wp_enqueue_script('wp-ba-comparsion', plugin_dir_url(__FILE__) . 'js/ba-comparsion.js', array('jquery'));
-	wp_enqueue_style( 'wp-ba-comparsion', plugin_dir_url(__FILE__) . 'css/style.css' );
+	wp_enqueue_script('js-js-mobile-js', plugin_dir_url(__FILE__) . 'js/js-mobile.js', array('jquery'));
+	wp_enqueue_script('js-ba-comparison-js', plugin_dir_url(__FILE__) . 'js/ba-comparsion.js', array('jquery'));
+	//wp_enqueue_style('css-css-mobile-css', plugin_dir_url(__FILE__) . 'css/css-mobile.css');
+	wp_enqueue_style( 'css-style-css', plugin_dir_url(__FILE__) . 'css/style.css' );
+	wp_enqueue_script('jqSw', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.4/jquery.touchSwipe.min.js');
+
+//
+wp_enqueue_script('swipeScr','https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.4/jquery.touchSwipe.min.js');
 	$result = '
 			<div class="row">
 				<div class="col-12 padding-0">
-					<input type="text" id="compareItems" value="201300000302,201700004903,201900001522">
-					<button id="comparsionStart" class="btn btn-default">Berechnen</button>
+					<input type="text" id="compareItems" value="201900001821,201400007688,201700001700,201800005002">
+					<button id="comparsionStart" class="btn btn-primary">Berechnen</button>
 				</div>
 			</div>
-			<div class="row small" id="comparisonListWrapper">
-				<div id="comparisonFeatures" class="col-4 padding-0 compareItemParent"></div>
 
-				<div id="comparisonCarousel1" class="col-4 padding-0 carousel slide" data-ride="carousel" data-interval=false>
-					<ol class="carousel-indicators"></ol>
+			<div class="row small" id="comparisonListHeader">
+				<div id="comparisonHeaderCarousel1" class="col-6 padding-0 carousel slide carousel-sync1"  data-ride="false" data-interval=false>
+					<div id="comparisonHeaderProducts1" class="carousel-inner"></div>
+					<ol class="carousel-indicators" style="position:relative;"></ol>
+				</div>
+
+				<div id="comparisonHeaderCarousel2" class="col-6 padding-0 carousel slide carousel-sync2"  data-ride="false" data-interval=false>
+					<div id="comparisonHeaderProducts2" class="carousel-inner"></div>
+					<ol class="carousel-indicators" style="position:relative;"></ol>
+				</div>
+			</div>
+			
+
+			<div class="row small" id="comparisonListWrapper" style="display:none;">
+				<div id="comparisonCarousel1" class="col-6 padding-0  carousel slide carousel-sync1" data-ride="false" data-interval=false>
+					<ol class="carousel-indicators" style="position:relative;"></ol>
 					<div id="comparisonProducts1" class="carousel-inner"></div>
-					<a class="carousel-control-prev" href="#comparisonCarousel1" role="button" data-slide="prev">
-				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				    <span class="sr-only">Previous</span>
-				  </a>
-				  <a class="carousel-control-next" href="#comparisonCarousel1" role="button" data-slide="next">
-				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-				    <span class="sr-only">Next</span>
-				  </a>
+				  	
+
+					  	<a  class="carousel-control-prev" href="#comparisonCarousel1" role="button" data-slide="prev">
+						    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						    <span class="sr-only">Previous</span>
+					  	</a>
+
+
+					  	<a class="carousel-control-next" href="#comparisonCarousel1" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+
+
 				</div>
 
-				<div id="comparisonCarousel2" class="col-4 padding-0 carousel slide" data-ride="carousel" data-interval=false>
-					<ol class="carousel-indicators"></ol>
+
+				<div id="comparisonCarousel2" class="col-6 padding-0 carousel slide carousel-sync2" data-ride=false data-interval=false>
+					<ol class="carousel-indicators" style="position:relative;"></ol>
 					<div id="comparisonProducts2" class="carousel-inner"></div>
-					<a class="carousel-control-prev" href="#comparisonCarousel1" role="button" data-slide="prev">
-						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-						<span class="sr-only">Previous</span>
-					</a>
-					<a class="carousel-control-next" href="#comparisonCarousel2" role="button" data-slide="next">
-						<span class="carousel-control-next-icon" aria-hidden="true"></span>
-						<span class="sr-only">Next</span>
-					</a>
+
+						<a class="carousel-control-prev" href="#comparisonCarousel2" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true""></span>
+							<span class="sr-only">Previous</span>
+						</a>
+
+						<a class="carousel-control-next" href="#comparisonCarousel2" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+
 				</div>
 			</div>
+
 	';
 	return $result;
 };
@@ -81,4 +110,4 @@ add_shortcode( 'comparsion', 'wp_ba_comparsion' );
 
 /* Plugin-Code OBERhalb dieser Zeile */
 
- ?>
+?>
